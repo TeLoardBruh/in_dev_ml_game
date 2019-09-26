44b784ddf9e16a9b49e4e7d0cc37d03e4afec84d
Rax <damnloardbruh@gmail.com>
2019-09-26 23:16:57 +0700

    test

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const PORT = 5000 || process.env.PORT;
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-
-
-
-app.set("view engine", "ejs");
-
-// here to add js into ejs
-app.use(express.static(path.join(__dirname, './')));
-
-app.get('/', (req, res) => res.render('index'));
-
-app.get('/app.ejs', (req, res) => res.render('app'));
-
-app.get('/web-cam.ejs', (req, res) => res.render('web-cam'));
-
-app.get('/train-db.ejs', (req, res) => res.render('train-db'));
-
-app.get('/trained-regress.ejs', (req, res) => res.render('trained-regress'));
-
-app.get('/save-load.ejs', (req, res) => res.render('save-load'));
-
-app.get('/knn.ejs', (req, res) => res.render('knn'));
-
-app.get('/knn-model.ejs', (req, res) => res.render('knn-model'));
-
-
-// app.get('/web-cam.ejs', (req, res) => res.render('web-cam'));
-
-// ========================================
-app.listen(PORT,()=>console.log(`Server up and running ${PORT}`));
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
